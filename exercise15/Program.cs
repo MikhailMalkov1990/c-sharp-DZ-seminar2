c9dfcbfadc1bff860656ceffddfeaacbc1e997a9
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-18 18:41:32 -0500

    added exercise15

---
+++ exercise15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("add week day:");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+
+if (weekDay == 6 || weekDay == 7 )
+{
+    Console.WriteLine("day off");
+}
+else if ( weekDay <1 || weekDay > 7 )
+{
+    Console.WriteLine("wrong day");
+}
+else 
+{
+    Console.WriteLine("weekday");
+}
