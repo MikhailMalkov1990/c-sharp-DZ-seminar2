9f708dde6789a292a84612739aa85be07da11940
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-18 18:39:00 -0500

    added exercise10

---
+++ exercise10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.WriteLine("add three digit num: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = (num % 100) / 10;
+// Console.WriteLine(result);
+
+Console.WriteLine("add three digit num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string stringNum = num.ToString();
+Console.WriteLine($"{stringNum[1]}");
