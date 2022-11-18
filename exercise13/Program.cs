ba96456b3fdbb16af52ea5d2e92c2d7d63929337
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-18 18:43:59 -0500

    added exercise13

---
+++ exercise13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("add your numbers:");
+int num = Convert.ToInt32(Console.ReadLine());
+string thirdNum = Convert.ToString(num);
+if (thirdNum.Length > 2)
+{
+    Console.WriteLine($"third digit number: " + thirdNum[2]);
+}
+else 
+{
+    Console.WriteLine("third digit number is missing");
+}
